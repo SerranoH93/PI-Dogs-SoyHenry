f4d802358eaa2a52d790419091e33a243790e4b3
Alejandro Serrano <92839762+SerranoH93@users.noreply.github.com>
2024-02-14 10:39:59 -0600

    Backend finished, start with frotend

--- api/src/models/Temperament.js
+++ api/src/models/Temperament.js
@@ -3,9 +3,10 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
     sequelize.define('temperament', {
         id: {
-            type: DataTypes.UUID,
-            primaryKey: true,
-            defaultValue: DataTypes.UUIDV4,
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
         },
         name: {
             type: DataTypes.STRING,
